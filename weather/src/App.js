dd5b72d5c39a7988f66939e0744c2331c0d2986a
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-01-21 10:16:28 +0530

    App Created

---
+++ weather/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+          <div className="search">
+            <form>
+              <input type="text" placeholder="Enter City Name"/>
+            </form>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
